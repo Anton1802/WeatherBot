1ad05b17345ab7c3f3e8708a07c0eeeb827349b2
Anton <anton.sup1802@gmail.com>
2023-02-01 18:18:05 +0300

    add configuration files

---
+++ config/data_bot.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "5885516304:AAH6haMHNEyHB1Ivb--m0SzYS1dH-F3Eku0"
+URL_API = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/" \
+          "timeline/?unitGroup=metric&key=4CWDMKRUDWAT7SDEUYABU6ZRD&contentType=json"
