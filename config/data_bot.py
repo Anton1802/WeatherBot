0e6cebc5055d3ee7cd89b0d4e0c9fc2da521ad2e
OpenSource <98132139+Anton1802@users.noreply.github.com>
2023-02-02 00:58:35 +0300

    Update data_bot.py
    Add documentation #3

--- config/data_bot.py
+++ config/data_bot.py
@@ -1,3 +1,8 @@
+"""
+Your configuration, to run the bot.
+Instead of "YOU_TOKEN_BOT" - specify your token.
+Instead of "YOU_API_KEY" - specify your Weather VisualCrossing API key
+"""
 BOT_TOKEN = "YOU_TOKEN_BOT"
 URL_API = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/" \
           "timeline/?unitGroup=metric&key=YOU_API_KEY&contentType=json"
